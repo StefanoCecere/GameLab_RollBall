2eea9fd72f7f5ab7711feea15af6d6e1c4a916d7
Stefano Cecere <stefano.cecere@krur.com>
2023-06-19 13:00:37 +0200

    updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
         // Assign the Rigidbody component to our private rb variable
         rb = GetComponent<Rigidbody>();
 
-        // Set the count to zero 
+        // Set the count to zero
         count = 0;
 
         // Run the SetCountText function to update the UI (see below)
@@ -43,18 +43,17 @@ public class PlayerController : MonoBehaviour
         // Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        // Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+        // Add a physical force to our Player rigidbody using our 'movement' Vector3 above,
         // multiplying it by 'speed' - our public player speed that appears in the inspector
         rb.AddForce(movement * speed);
     }
 
-    // When this game object intersects a collider with 'is trigger' checked, 
+    // When this game object intersects a collider with 'is trigger' checked,
     // store a reference to that collider in a variable named 'other'..
     void OnTriggerEnter(Collider other)
     {
         // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
+        if (other.gameObject.CompareTag("Pick Up")) {
             // Make the other game object (the pick up) inactive, to make it disappear
             other.gameObject.SetActive(false);
 
@@ -75,8 +74,7 @@ public class PlayerController : MonoBehaviour
         countText.text = "Score: " + count.ToString();
 
         // Check if our 'count' is equal to or exceeded 12
-        if (count >= ScoreToWin)
-        {
+        if (count >= ScoreToWin) {
             // Set the text value of our 'winText'
             winTextObject.SetActive(true);
         }
